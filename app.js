074ffd33bc82c67f546750f194ab728abcd93141
Justin Murrah <jdmurrah@gmail.com>
2022-11-08 22:46:43 -0600

    initial commit, some arrays and prep

---
+++ app.js
@@ -0,0 +1,38 @@
+var friend = ["Jen", "Jamie", "Sequoya", "Logan", "Emily"];
+var location = [
+  "Crack House",
+  "river water",
+  "Nottingham Brothel",
+  "Dog Pound",
+  "Mommy's bedroom swing",
+  "Skunk's Den",
+  "middle of the damn field",
+  "Metaverse Server Room",
+  "Secret Garden",
+  "good Barbershop",
+];
+var weapon = [
+  "the coat hanger",
+  "the water gun",
+  "an elephant tusk",
+  "1/2 of a kamehameha themed handgun",
+  "a kitchen knife",
+  "boredom",
+  "IBS",
+  "laser eyes",
+  "the weight of Elon's wealth",
+  "a lightsaber",
+  "a summoned asteroid",
+  "250 hours of a kitten's meow",
+  "a broken keyboard",
+  "rusty wires",
+  "the banister decorations",
+  "a vacuum sealed pool noodle",
+  "hypnosis",
+  "the meanest glare ever given",
+  "an Xbox controller",
+  "the ceiling fan blade",
+];
+for (var i = 0; i < 100; i++) {
+  var h3 = $("body").append("<h3></h3>");
+}
