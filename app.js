47959ffd2b81270811cf27ba0fb34e6eebd6855b
Justin Murrah <jdmurrah@gmail.com>
2022-11-09 10:07:08 -0600

    h3's are there random logic is there, needs click event added for alert.

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
+//$(document).ready(function () {
 var friend = ["Jen", "Jamie", "Sequoya", "Logan", "Emily"];
-var location = [
-  "Crack House",
-  "river water",
-  "Nottingham Brothel",
-  "Dog Pound",
-  "Mommy's bedroom swing",
-  "Skunk's Den",
-  "middle of the damn field",
-  "Metaverse Server Room",
-  "Secret Garden",
-  "good Barbershop",
-];
+
 var weapon = [
   "the coat hanger",
   "the water gun",
@@ -33,10 +23,29 @@ var weapon = [
   "an Xbox controller",
   "the ceiling fan blade",
 ];
+var murderSite = [
+  "Crack House",
+  "river water",
+  "Nottingham Brothel",
+  "Dog Pound",
+  "Mommy's bedroom swing",
+  "Skunk's Den",
+  "middle of the damn field",
+  "Metaverse Server Room",
+  "Secret Garden",
+  "good Barbershop",
+];
 for (var i = 0; i < 100; i++) {
-  var h3;
-
-  if (i < 100);
-
-  h3 = $("body").append("<h3></h3>");
+  //var div = $("h3").append("<div>some text</div>");
+  var h3 = $("body").append("<h3>", "Accusation " + [i], "</h3>");
+  h3.click(randomSelector[i]);
 }
+
+function randomSelector() {
+  var friendSelect = [Math.floor(Math.random()).friend.length];
+  var weaponSelect = [Math.floor(Math.random()).weapon.length];
+  var murderSiteSelect = [Math.floor(Math.random()).murderSite.length];
+}
+//});
+
+//[Math.floor(Math.random())];
